4d16c011205e4454e0a3eda91c3c558cdfd5f840
liuguisheng <codeliuguisheng@163.com>
2022-01-05 17:08:49 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,43 +1,78 @@
+
 module.exports = {
-    title: 'TypeScript4 文档',
+    title: '凤凰城下的小码农',
     description: 'TypeScript4 最新官方文档翻译',
     theme: 'reco',
-    base: '/qisi007.github.io/',
+    // base: './',
     locales: {
         '/': {
             lang: 'zh-CN'
         }
     },
+    head: [
+
+        [
+          'link',
+          {
+            rel: 'shortcut icon',
+            type: 'image/x-icon',
+            href: `/logo.png`,
+          },
+        ],
+    ],
     themeConfig: {
         subSidebar: 'auto',
         nav: [
             { text: '首页', link: '/' },
+            { text: 'java', link: '/java/java1'},
+            { text: '前端', link: '/js/js1'},
             {
-                text: '冴羽的 JavaScript 博客',
+                text: '我的博客',
                 items: [
                     { text: 'Github', link: 'https://github.com/mqyqingfeng' },
                     { text: '掘金', link: 'https://juejin.cn/user/712139234359182/posts' }
                 ]
             }
         ],
-        sidebar: [
-            {
-                title: '欢迎学习',
-                path: '/',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "学前必读", path: "/" }
-                ]
-            },
-            {
-                title: "基础学习",
-                path: '/handbook/ConditionalTypes',
-                collapsable: false, // 不折叠
-                children: [
-                    { title: "条件类型", path: "/handbook/ConditionalTypes" },
-                    { title: "泛型", path: "/handbook/Generics" }
-                ],
-            }
-        ]
+        sidebar: {
+            '/java/': [
+                {
+                    title: 'java1',
+                    path: '/java/java1'
+                },
+                {
+                    title: 'java2',
+                    path: '/java/java2'
+                }
+            ],
+            '/js/': [
+                {
+                    title: 'js1',
+                    path: '/js/js1'
+                },
+                {
+                    title: 'js2',
+                    path: '/js/js2'
+                }
+            ]
+        }
+        
+        
+        // [
+        //     {
+        //         title: '欢迎学习',
+        //         path: '/',
+        //         collapsable: false
+        //     },
+        //     {
+        //         title: "基础学习",
+        //         path: '/handbook/ConditionalTypes',
+        //         collapsable: false, // 不折叠
+        //         children: [
+        //             { title: "条件类型", path: "/handbook/ConditionalTypes" },
+        //             { title: "泛型", path: "/handbook/Generics" }
+        //         ],
+        //     }
+        // ]
     }
 }
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,21 @@
-nihao
+<html>
+    <div class="banner">
+        <img class="img" src="/logo.png"/>
+        <div class="line"></div>
+        <div class="sub-title">一个被敲代码耽误的大艺术家！</div>
+    </div>
+    <div class="hot-content"></div>
+    <div class="source"></div>
+</html>
+<style>
+    .page {
+        padding-top: 3rem;
+    }
+    .page .page-title {
+        display: none;
+    }
+    .content__default:not(.custom) {
+        max-width: 100%;
+        padding: 0;
+    }
+</style>
