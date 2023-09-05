e53b7bc98cf7f7b715b535aacfb6376e71097bee
liuguisheng <codeliuguisheng@163.com>
2023-09-05 17:53:07 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,32 @@
 #git push origin main
 
 # 生成静态文件
-pnpm docs:build
+echo "开始打包"
 
 sleep 3s
 
-cp CNAME docs/.vuepress/dist
+pnpm docs:build
+
+echo "打包完成，等待复制站点文件"
+
+sleep 3s
+
+cp CNAME docs/.vitepress/dist/
+
+echo "文件复制完成，准备发布"
+
+sleep 3s
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-
-
 git init
 git add .
 git commit -m 'deploy'
+
+echo "开始发布"
+
+sleep 3s
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:qisi007/qisi007.github.io.git master:gh-pages
